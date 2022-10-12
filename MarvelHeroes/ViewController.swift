6c9ed203b730b5f8863097ac49c4f4b0197ced93
Krystyna Kruchkovska <kruchkovskakr@gmail.com>
2022-10-12 19:14:46 +0200

    Initial Commit

---
+++ MarvelHeroes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MarvelHeroes
+//
+//  Created by Paweł on 12/10/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
